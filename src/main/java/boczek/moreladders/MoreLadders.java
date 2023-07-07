e7b2fe2932584b312e323104dd33c7aaa6e334fd
boczek <bartekkoraliki@gmail.com>
2023-07-07 10:22:51 +0200

    beta -1.0.-11

--- src/main/java/boczek/moreladders/MoreLadders.java
+++ src/main/java/boczek/moreladders/MoreLadders.java
@@ -1,2 +1,16 @@
-public class MoreLadders {
+package boczek.moreladders;
+
+import boczek.moreladders.block.BlockLadders;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class MoreLadders implements ModInitializer {
+    public static final String MOD_ID = "moreladders";
+    public static final Logger LOGGER = LoggerFactory.getLogger("moreladders");
+    @Override
+    public void onInitialize() {
+        BlockLadders.registerModBlocks();
+    }
 }
---
+++ src/main/java/boczek/moreladders/item/MoreLaddersGroup.java
@@ -0,0 +1,21 @@
+package boczek.moreladders.item;
+
+import boczek.moreladders.MoreLadders;
+import boczek.moreladders.block.BlockLadders;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class MoreLaddersGroup {
+    public static ItemGroup MORE_LADDERS_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(MoreLadders.MOD_ID, "more-ladders"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.more-ladders")).icon(() -> new ItemStack(BlockLadders.SPRUCE_LADDER))
+                    .entries((displayContext, entries) -> {
+                        // Items to add
+                        entries.add(BlockLadders.SPRUCE_LADDER);
+                    }).build());
+    public static void load
+}
