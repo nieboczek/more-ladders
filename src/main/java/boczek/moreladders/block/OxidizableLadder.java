e137f3427236e2ec2fe5ca901ec4b0c973e31117
NieBoczek <106301320+Bocz3k@users.noreply.github.com>
2024-04-12 15:29:41 +0200

    i hate merges

--- src/main/java/boczek/moreladders/block/OxidizableLadder.java
+++ src/main/java/boczek/moreladders/block/OxidizableLadder.java
@@ -101,12 +101,7 @@ public class OxidizableLadder extends LadderBlock implements Oxidizable {
 
     @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        float f = 0.05688889F;
-        if (random.nextFloat() < 0.05688889F) {
-            this.tryDegrade(state, world, pos, random).ifPresent((degraded) -> {
-                world.setBlockState(pos, degraded);
-            });
-        }
+        this.tickDegradation(state, world, pos, random);
     }
 
     @Override
