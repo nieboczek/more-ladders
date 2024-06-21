46d61e50aa83a45237db70c170d2bc8e444456ac
NieBoczek <bartekkoraliki@gmail.com>
2024-06-21 12:16:41 +0200

    this now works for some reason

--- src/main/java/boczek/moreladders/block/OxidizableLadder.java
+++ src/main/java/boczek/moreladders/block/OxidizableLadder.java
@@ -27,12 +27,7 @@ public class OxidizableLadder extends LadderBlock implements Oxidizable {
 
     @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        // for some reason this.tickDegradation doesn't work, so I stole the source code
-        if (random.nextFloat() < 0.05688889F) {
-            this.tryDegrade(state, world, pos, random).ifPresent((degraded) -> {
-                world.setBlockState(pos, degraded);
-            });
-        }
+        this.tickDegradation(state, world, pos, random);
     }
 
     @Override
